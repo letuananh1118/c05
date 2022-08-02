1a05df5f5731b7ab37f8a57ad127dc93fab4cc0c
letuananh1118 <tuananhlee1811@gmail.com>
2022-08-02 22:08:19 +0700

    dd

---
+++ c05/js/child.js
@@ -0,0 +1,5 @@
+var startItem=document.getElementsByTagName('ul');
+var firstItem=startItem.firstChild;
+var lastItem=startItem.lastChild;
+firstItem.className='complete';
+lastItem.className='cool';
